e7bc781d26c773beea806655126bb496d8d7f446
Dragonlord1005 <68302450+Dragonlord1005@users.noreply.github.com>
2022-09-27 18:36:59 +0000

    hi

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,3 @@
-#[cfg(feature = "smawk")] {
-let text = "textwrap: an efficient and powerful library for wrapping text.";
-assert_eq!(
-    textwrap::wrap(text, 28),
-    vec![
-        "textwrap: an efficient",
-        "and powerful library for",
-        "wrapping text.",
-    ]
-);
+fn main() {
+      println!("Hello, world!");
 }
